70d05e21d18d2161fd7345f334de2ee8d812eb44
DilshanM99 <dilshan1999madura@gmail.com>
2024-12-12 17:13:14 +0530

    payroll processign veiw changes

--- application/views/payroll_processing/index.php
+++ application/views/payroll_processing/index.php
@@ -1,18 +1,15 @@
 <div class="col-8">
     <div class="header">
         <h2>Payroll Processing</h2>
-        <!-- <a href="<?php echo site_url('payroll_processing/create'); ?>" class="btn-add">
-            <i class="fas fa-plus-square"></i> Add Payroll Process
-        </a> -->
-        <a href="<?php echo base_url('payroll_processing/create'); ?>" class="btn btn-primary">Create New Record</a>
-        
+        <a href="<?php echo base_url('payroll_processing/create'); ?>" class="btn-add"><i
+                class="fas fa-plus-square"></i> Create New Record</a>
+
     </div>
 
     <div class="table-container">
         <table>
             <thead>
                 <tr>
-                    <th>ID</th>
                     <th>Processed By</th>
                     <th>Processed Date</th>
                     <th>Month</th>
@@ -23,7 +20,6 @@
             <tbody>
                 <?php foreach ($payroll_processing as $row): ?>
                     <tr>
-                        <td><?php echo $row->id; ?></td>
                         <td><?php echo $row->processed_by; ?></td>
                         <td><?php echo date('d-m-Y', strtotime($row->processed_date)); ?></td>
                         <td><?php echo date('F Y', strtotime($row->month)); ?></td>
